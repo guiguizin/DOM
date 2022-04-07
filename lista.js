5cfc1da3309761f1e0f05fcc91dfe9f0a033438f
guiguizin <guilhermehenriqueoliveiras10@gmail.com>
2022-04-07 10:12:50 -0300

    DOM

--- lista.js
+++ lista.js
@@ -1,10 +1,31 @@
+function DeleteItem(){
+    console.log(this.parentElement)
+    this.parentElement.remove()
+}
+
+function CriarBotaoDelete(){
+    const botao_delete =  document.createElement('button')
+    botao_delete.classList.add("btn", "btn-danger")
+    botao_delete.innerHTML = "Delete"
+    botao_delete.type = "button"
+    botao_delete.addEventListener("click", DeleteItem);
+
+    return botao_delete
+}
+
 function Submit(){
     const lista = document.querySelector('[data-task]')
-    const novo_item_lista = document.createElement("li")
     const valor = document.querySelector('[data-form-input]')
-    
-    novo_item_lista.innerHTML=valor.value
+
+    const novo_item_lista = document.createElement("li")
+    novo_item_lista.className = "list-group-item";
+
+    novo_item_lista.innerHTML = valor.value
+ 
+    novo_item_lista.appendChild(CriarBotaoDelete())
     lista.appendChild (novo_item_lista)
+
+    document.getElementById("item").value = ""
 }
 
 
